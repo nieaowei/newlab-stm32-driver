a7cc3648273a0704a5d993144ec65b68468247ed
nieaowei <nieaowei@gmail.com>
2019-10-20 14:12:05 +0800

    May be the last submission.

--- 温度光照模块/TEMP_LIGHT/Objects/temp_light.d
+++ 温度光照模块/TEMP_LIGHT/Objects/temp_light.d
@@ -4,10 +4,10 @@
 .\objects\temp_light.o: .\USER\SYSTEM\sys.h
 .\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
 .\objects\temp_light.o: .\RTE\_TEMP_LIGHT\RTE_Components.h
-.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm3.h
-.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
-.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
+.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
 .\objects\temp_light.o: .\RTE\Device\STM32F103VE\stm32f10x_conf.h
 .\objects\temp_light.o: C:\Users\nieaowei\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_adc.h
